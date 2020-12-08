d8ade509ae1d05288ea78abfb9ec830994837176
zxy <948339426@qq.com>
2020-12-08 15:38:15 +0800

    Initial commit

---
+++ src/main/java/com/zxy/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.zxy.web;
+
+import com.zxy.NotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        return "blog";
+    }
+}
